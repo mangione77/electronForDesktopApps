67d8720bc316b6999138b3250402e3029bbb176d
Pablo <pablo@aktek.io>
2018-02-04 16:48:10 +0100

    Showing file metadata to user

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,10 +1,22 @@
 const electron = require('electron')
-const path = require('path')
-const { app, BrowserWindow } = electron
+const ffmpeg = require('fluent-ffmpeg')
+const { app, BrowserWindow, ipcMain } = electron
+
+let mainWindow
 
 app.on('ready', () => {
-    let mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({})
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
 
+ipcMain.on('video:submit', (event,path) => {
+
+    ffmpeg.ffprobe(path, (err,metadata) => {
+        if (err) console.error(err)
+
+        let duration = metadata.format.duration 
+
+        mainWindow.webContents.send('video:metadata', duration)
+    })
+})
 
